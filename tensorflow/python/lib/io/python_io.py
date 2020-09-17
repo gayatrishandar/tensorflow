74c40e93d5f526fe11fe1def578ef7f9389a8a67
g3gayatree <gshandar@andrew.cmu.edu>
2020-09-16 21:51:36 -0400

    added import

--- tensorflow/python/lib/io/python_io.py
+++ tensorflow/python/lib/io/python_io.py
@@ -18,7 +18,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import numpy
 # go/tf-wildcard-import
 # pylint: disable=wildcard-import
 from tensorflow.python.lib.io.tf_record import *
